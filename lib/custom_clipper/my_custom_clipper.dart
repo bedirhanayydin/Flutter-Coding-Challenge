30b4ed56062b5c66120a0ee95b5bf2abd71be579
bedirhhanaydin <bedirhhanaydin@gmail.com>
2022-08-03 11:20:08 +0300

    custom clipper added.

--- lib/custom_clipper/my_custom_clipper.dart
+++ lib/custom_clipper/my_custom_clipper.dart
@@ -7,7 +7,7 @@ class MyCustomClipper extends CustomClipper<Path> {
     path.quadraticBezierTo(size.width/4, size.height-60, size.width/2, size.height-30);
     path.quadraticBezierTo(3/4*size.width, size.height, size.width, size.height-40);
     path.lineTo(size.width, 0);*/
-    double radius = 10.0;
+    double radius = 20.0;
 
     Path path = Path();
     path.lineTo(size.width, 0);
@@ -17,13 +17,13 @@ class MyCustomClipper extends CustomClipper<Path> {
     // path.quadraticBezierTo(size.width - 40, 40, size.width - 60, 40);
     path.arcToPoint(
       Offset(size.width - 60, 40),
-      radius: const Radius.circular(5 * 5),
+      radius: Radius.circular(radius),
       clockwise: false,
     );
     path.lineTo(60, 40);
     path.arcToPoint(
       const Offset(40, 60),
-      radius: const Radius.circular(5 * 5),
+      radius: Radius.circular(radius),
       clockwise: false,
     );
     path.lineTo(40, size.height);
